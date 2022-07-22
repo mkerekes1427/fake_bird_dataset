9afc57e099f03a2b3b09d4703d0c192b9a89ebac
mkerekes1427 <73243252+mkerekes1427@users.noreply.github.com>
2022-07-22 17:44:08 -0400

    Add files via upload

--- all_queries.sql
+++ all_queries.sql
@@ -105,5 +105,118 @@ ORDER BY num_observations DESC
 LIMIT 5;
 */
 
+-- ------------------------------- Break--------------------------------------------- ---
+
+-- Find the number of endangered birds in the west (Only 41 states in dataset.)
+
+/*SELECT 
+    COUNT(DISTINCT s.id) AS num_species_endangered
+FROM
+    observations o
+        JOIN
+    bird_species s ON o.bird_id = s.id
+        JOIN
+    areas a ON o.area_id = a.id
+WHERE
+    a.state IN ('California' , 'Oregon',
+        'Washington',
+        'Nevada',
+        'Utah',
+        'Arizona',
+        'New Mexico',
+        'Montana',
+        'Idaho',
+        'Colorado',
+        'Wyoming')
+        AND s.endangered = 'TRUE';
+*/
+
+-- -------------------------------------- Break ------------------------------- --
+
+-- Find the most popular bird for each state (based on the greatest bird count for a species in a state)
+
+/*
+with popular_birds_by_state as(
+SELECT 
+    s.name, a.state, SUM(o.species_count) AS num_spotted,
+    Rank() over (partition by state order by SUM(o.species_count) Desc) as bird_rank
+FROM
+    observations o
+        JOIN
+    bird_species s ON o.bird_id = s.id
+        JOIN
+    areas a ON o.area_id = a.id
+GROUP BY s.name, a.state)
+
+SELECT 
+    name, state, num_spotted
+FROM
+    popular_birds_by_state
+WHERE
+    bird_rank = 1
+    order by state;
+    
+*/
+
+-- --------------------------------------- Break --------------------------------------
 
 
+-- Find the average flight speed and weight for birds that have European or Eurasian in their names
+-- compared to all other birds.
+
+/*
+(SELECT 
+    ROUND(AVG(flight_speed), 2) AS avg_flight_speed,
+    ROUND(AVG(weight_ounces), 2) AS avg_weight,
+    'European' AS nationality
+FROM
+    bird_species
+WHERE
+    name LIKE '%European%'
+        OR name LIKE '%Eurasian%') UNION (SELECT 
+    ROUND(AVG(flight_speed), 2) AS avg_flight_speed,
+    ROUND(AVG(weight_ounces), 2) AS avg_weight,
+    'American' AS nationality
+FROM
+    bird_species
+WHERE
+    name NOT LIKE '%European%'
+        AND name NOT LIKE '%Eurasian%');
+*/
+
+-- ------------------------------------------Break--------------------------------------- --
+
+-- Find the total number of birds that were heard but not photographed, photographed but not heard,
+-- photographed and heard, and neither photographed or heard.
+
+/*
+SELECT 
+    CASE
+        WHEN made_sound = 'TRUE' AND camera = 'None' THEN 'Heard but no pic'
+        WHEN
+            made_sound = 'FALSE'
+                AND camera <> 'None'
+        THEN
+            'Not heard but pic'
+        WHEN made_sound = 'TRUE' AND camera <> 'None' THEN 'Heard and pic'
+        WHEN made_sound = 'FALSE' AND camera = 'None' THEN 'Not heard and no pic'
+    END AS observation_category,
+    SUM(species_count) AS num_observed
+FROM
+    observations
+GROUP BY observation_category
+ORDER BY num_observed DESC;
+*/
+
+--  -----------------------------Break-------------------------------------
+
+-- Find total number of birds photographed per camera.
+
+/* 
+SELECT 
+    camera, SUM(species_count) num_photographed
+FROM
+    observations
+group by camera
+order by num_photographed DESC;
+*/
